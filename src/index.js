eb27936a40309723e1e80dd768bc8fa1f4eb4015
Ran <96964503+rndall@users.noreply.github.com>
2025-03-29 17:01:31 +0800

    Add function to fetch weather data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import "./assets/reset.css"
+import key from "./key"
+
+async function fetchWeatherData(location, isCelcius) {
+  const base =
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
+  const url = `${base}${location}?key=${key}${
+    isCelcius ? "&unitGroup=metric" : ""
+  }`
+  try {
+    const response = await fetch(url)
+
+    if (!response.ok) throw new Error(`Response status: ${response.status}`)
+
+    const data = await response.json()
+    console.log(data)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+fetchWeatherData("london", true)
